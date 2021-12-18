52afdfedae02cc192a78fd6bc921191bbc0cf848
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-18 14:52:00 +0100

    user controller well done for register end point

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,13 +2,16 @@ const Users = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
 const userCtrl = {
+  //register end point
   register: async (req, res) => {
     try {
+      //storing value of client side
       const { name, email, password } = req.body;
+      //check if user already exist in database
       const user = await Users.findOne({ email });
       if (user)
         return res.status(400).json({ msg: "This email is already in user." });
-
+      // password should be more than 6 char
       if (password.length < 6)
         return res
           .status(400)
@@ -21,6 +24,7 @@ const userCtrl = {
         email,
         password: passwordHash,
       });
+      //saving user in database
       await newUser.save();
       res.json({ msg: "Register success" });
     } catch (err) {
