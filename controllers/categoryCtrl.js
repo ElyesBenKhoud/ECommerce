4d2e12a681e92cc741dfc76810dba8391c2865ee
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-19 21:07:42 +0100

    1:35:50 done create category

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -11,7 +11,17 @@ const categoryCtrl = {
   },
   createCategory: async (req, res) => {
     try {
-      res.json("admin success");
+      //if user have role = 1 --> means he is an admin
+      //only admin can create,delete and update category
+      const { name } = req.body;
+      const category = await Category.find({ name });
+      if (category)
+        return res.status(400).json({ msg: "this category already exists" });
+
+      const newCategory = new Category({ name });
+
+      await newCategory.save();
+      res.json({ msg: "created new category" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
