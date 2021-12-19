2ad5d177c7f4a21a55c23e007c944d117e426837
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-19 20:43:30 +0100

    category schema added

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,2 +1,17 @@
+const mongoose = require("mongoose");
 
-const 
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
