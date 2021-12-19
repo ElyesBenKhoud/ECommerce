0669b6ea2b3fa4d1f59246f35052fcaed403046d
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-19 20:57:18 +0100

    getCategories function done

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -1,6 +1,13 @@
+const Category = require("../models/categoryModel");
+
 const categoryCtrl = {
   getCategories: async (req, res) => {
-    res.json("category test ctrl");
+    try {
+      const categories = await Category.find();
+      res.json(categories);
+    } catch (error) {
+      return res.status(404).json({ msg: error.message });
+    }
   },
 };
 
