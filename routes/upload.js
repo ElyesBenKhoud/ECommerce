19e0101f2f6db7e2c80f2fa87c2f2aa8eac262c1
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-22 19:23:27 +0100

    cloudinary config

--- routes/upload.js
+++ routes/upload.js
@@ -5,3 +5,11 @@ const cloudinary = require("cloudinary");
 const auth = require("../middleware/auth");
 
 const authAdmin = require("../middleware/authAdmin");
+
+//upload img using cloudinary
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
