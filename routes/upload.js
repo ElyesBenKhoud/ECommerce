ca0ec3fb61dda08190bd00eb130ac11d05225a67
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-22 19:34:45 +0100

    file format of cloudinary handled

--- routes/upload.js
+++ routes/upload.js
@@ -18,9 +18,17 @@ router.post("/upload", (req, res) => {
   try {
     console.log(req.files);
     //in case nothing uploaded
-    if (!req.files || Object.keys(req.files).length === 0) {
+    if (!req.files || Object.keys(req.files).length === 0)
       return res.status(400).send("no files were uploaded");
-    }
+
+    const file = req.files.file;
+    //1 mo size 1024*1024
+    if (file.size > 1024 * 1024)
+      return res.status(400).json({ msg: "size too large" });
+    //handleing type of img
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png")
+      return res.status(400).json({ msg: "file format is incorrect." });
+
     res.json("img received");
   } catch (error) {
     res.status(500).json({ msg: error.message });
