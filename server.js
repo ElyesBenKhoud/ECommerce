3cdac96dd2a8e9d37769c2eb1a50eee999b95c15
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-22 19:25:55 +0100

    handeling error in the req cloudinary

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(
 //Routes
 app.use("/user", require("./routes/userRouter"));
 app.use("/api", require("./routes/categoryRouter"));
+app.use("/api", require("./routes/upload"));
 
 //connect tp MongoDB
 const URI = process.env.MONGODB_URL;
