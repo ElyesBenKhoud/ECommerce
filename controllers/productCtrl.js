f2557c72742d5fd9cf806bb05d5d471b4cbab6cd
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-23 11:43:43 +0100

    controller skeleton added for CRUD and linked to routes of products

---
+++ controllers/productCtrl.js
@@ -0,0 +1,30 @@
+const Products = require("../models/productModel");
+
+const productCtrl = {
+  getProducts: async (req, res) => {
+    try {
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  createProduct: async (req, res) => {
+    try {
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  deleteProduct: async (req, res) => {
+    try {
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  updateProduct: async (req, res) => {
+    try {
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+};
+
+module.exports = productCtrl;
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-router.route("/products").get().post();
+const productCtrl = require("../controllers/productCtrl");
 
-router.route("/products/:id").delete().put();
+router
+  .route("/products")
+  .get(productCtrl.getProducts)
+  .post(productCtrl.createProduct);
+
+router
+  .route("/products/:id")
+  .delete(productCtrl.deleteProduct)
+  .put(productCtrl.updateProduct);
 
 module.exports = router;
