9b2406f0f5a0d3a89005a0400ef0754adb6d1cab
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-23 20:59:58 +0100

    ignoring files

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,9 +1,35 @@
 import React, { useState, useContext } from "react";
 import { GlobalState } from "../../GlobalState";
+import Menu from "./icon/menu.svg";
+import Close from "./icon/close.svg";
+import Cart from "./icon/cart.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const value = useContext(GlobalState);
-  return <div>{value}</div>;
+  return (
+    <Header>
+      <div className="menu">
+        <img src={Menu} alt="" width="30" />
+      </div>
+
+      <div className="logo">
+        <h1>
+          <Link to="/"> DevAt Shop</Link>
+        </h1>
+      </div>
+
+      <ul>
+        <li>
+          <Link to="/"> Products</Link>
+        </li>
+
+        <li>
+          <Link to="/login"> Login Or Register</Link>
+        </li>
+      </ul>
+    </Header>
+  );
 };
 
 export default Header;
